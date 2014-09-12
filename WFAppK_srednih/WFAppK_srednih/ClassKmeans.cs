9768c93bd9f8a824a9752e877d9d49523d8e0023
Muonish <breshko314@gmail.com>
2014-09-12 17:52:15 +0300

    2nd

---
+++ WFAppK_srednih/WFAppK_srednih/ClassKmeans.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace WFAppK_srednih
+{
+    class ClassKmeans
+    {
+        int Npoint;
+        int Nclass;
+        int WIDTH;
+        int HEIGHT;
+
+        public struct PointInfo
+        {
+            public Point coord;  
+            public int father;
+        }
+
+        public ClassKmeans(int npoint, int nclass, int width, int height)
+        {
+            Npoint = npoint;
+            Nclass = nclass;
+            WIDTH = width;
+            HEIGHT = height;
+        }
+
+        private void RandomPoint()
+        {
+            Random RandomNumber = new Random();
+            PointInfo[] vectorPoint = new PointInfo[Npoint];
+
+            for (int i = 0; i < Npoint; i++)
+            {
+                vectorPoint[i].coord = new Point(RandomNumber.Next(1, WIDTH), RandomNumber.Next(1, HEIGHT));
+                vectorPoint[i].father = RandomNumber.Next(0, Npoint-1);
+            }
+            for (int i = 0; i < Npoint; i++)
+            {
+                Pen pen = new Pen(Color.Black);
+                Graphics GpanelHolst;
+                GpanelHolst = panelHolst.CreateGraphics();
+                
+            }
+        }
+    }
+
+}
--- WFAppK_srednih/WFAppK_srednih/Form1.cs
+++ WFAppK_srednih/WFAppK_srednih/Form1.cs
@@ -19,7 +19,38 @@ namespace WFAppK_srednih
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
+            int Npoint = 0;
+            int Nclass = 0;
 
+            while (comboBox1.Text == "" || comboBox2.Text == "")
+            {
+                MessageBox.Show("Enter the numbers.");
+               /* Graphics GpanelHolst;
+                Point begin = new Point(5,5);
+                Point end = new Point(50,50);
+                Pen pen = new Pen(Color.Black);
+
+                GpanelHolst = panelHolst.CreateGraphics();
+                GpanelHolst.DrawLine(pen, begin, end);*/
+            }
+            try
+            {
+                Npoint = Convert.ToInt32(comboBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Error! Out of int32!");
+            }
+            try
+            {
+                Nclass = Convert.ToInt32(comboBox2.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Error! Out of int32!");
+            }
+
+            
         }
     }
 }
