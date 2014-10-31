849762f5cce8b583243c154df57648fbacc8254d
Muonish <breshko314@gmail.com>
2014-10-31 20:59:56 +0300

    Potentials_finally

---
+++ WFAppPotentials/WFAppPotentials/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace WFAppPotentials
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl = new ZedGraph.ZedGraphControl();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1stProbability = new System.Windows.Forms.Label();
+            this.label2ndProbability = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBoxFalseAlarm = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMissingError = new System.Windows.Forms.TextBox();
+            this.textBoxTotalError = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl
+            // 
+            this.zedGraphControl.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.zedGraphControl.Location = new System.Drawing.Point(-1, 82);
+            this.zedGraphControl.Name = "zedGraphControl";
+            this.zedGraphControl.ScrollGrace = 0D;
+            this.zedGraphControl.ScrollMaxX = 0D;
+            this.zedGraphControl.ScrollMaxY = 0D;
+            this.zedGraphControl.ScrollMaxY2 = 0D;
+            this.zedGraphControl.ScrollMinX = 0D;
+            this.zedGraphControl.ScrollMinY = 0D;
+            this.zedGraphControl.ScrollMinY2 = 0D;
+            this.zedGraphControl.Size = new System.Drawing.Size(845, 452);
+            this.zedGraphControl.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(302, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1stProbability
+            // 
+            this.label1stProbability.AutoSize = true;
+            this.label1stProbability.BackColor = System.Drawing.Color.Azure;
+            this.label1stProbability.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1stProbability.Location = new System.Drawing.Point(186, 17);
+            this.label1stProbability.Name = "label1stProbability";
+            this.label1stProbability.Size = new System.Drawing.Size(110, 20);
+            this.label1stProbability.TabIndex = 2;
+            this.label1stProbability.Text = "1st probability:";
+            // 
+            // label2ndProbability
+            // 
+            this.label2ndProbability.AutoSize = true;
+            this.label2ndProbability.BackColor = System.Drawing.Color.Azure;
+            this.label2ndProbability.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2ndProbability.Location = new System.Drawing.Point(181, 45);
+            this.label2ndProbability.Name = "label2ndProbability";
+            this.label2ndProbability.Size = new System.Drawing.Size(115, 20);
+            this.label2ndProbability.TabIndex = 3;
+            this.label2ndProbability.Text = "2nd probability:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(302, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCalculate.Location = new System.Drawing.Point(442, 12);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(182, 57);
+            this.buttonCalculate.TabIndex = 5;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PowderBlue;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBoxTotalError);
+            this.panel1.Controls.Add(this.textBoxMissingError);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxFalseAlarm);
+            this.panel1.Location = new System.Drawing.Point(0, 505);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(841, 152);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Azure;
+            this.panel2.Controls.Add(this.label1stProbability);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.label2ndProbability);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(841, 152);
+            this.panel2.TabIndex = 7;
+            // 
+            // textBoxFalseAlarm
+            // 
+            this.textBoxFalseAlarm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxFalseAlarm.Location = new System.Drawing.Point(442, 47);
+            this.textBoxFalseAlarm.Name = "textBoxFalseAlarm";
+            this.textBoxFalseAlarm.Size = new System.Drawing.Size(182, 26);
+            this.textBoxFalseAlarm.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PowderBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(232, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "False alarm probability:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.PowderBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(224, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Missing error probability:";
+            // 
+            // textBoxMissingError
+            // 
+            this.textBoxMissingError.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxMissingError.Location = new System.Drawing.Point(442, 77);
+            this.textBoxMissingError.Name = "textBoxMissingError";
+            this.textBoxMissingError.Size = new System.Drawing.Size(182, 26);
+            this.textBoxMissingError.TabIndex = 5;
+            // 
+            // textBoxTotalError
+            // 
+            this.textBoxTotalError.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxTotalError.Location = new System.Drawing.Point(442, 107);
+            this.textBoxTotalError.Name = "textBoxTotalError";
+            this.textBoxTotalError.Size = new System.Drawing.Size(182, 26);
+            this.textBoxTotalError.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.PowderBlue;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(205, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Total error of classification:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 653);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.zedGraphControl);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormMain";
+            this.Text = "Potential method";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1stProbability;
+        private System.Windows.Forms.Label label2ndProbability;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxTotalError;
+        private System.Windows.Forms.TextBox textBoxMissingError;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFalseAlarm;
+    }
+}
+
---
+++ WFAppPotentials/WFAppPotentials/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace WFAppPotentials
+{
+    public partial class FormMain : Form
+    {
+        public const int MAXPOINTS = 10000;
+        public static GraphPane pane;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            pane = zedGraphControl.GraphPane;
+            pane.Title.Text = "";
+            pane.XAxis.Title.Text = "X";
+            pane.YAxis.Title.Text = "P";
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar < '0' || e.KeyChar > '9')
+                if (e.KeyChar != 8)
+                    if (e.KeyChar != ',')
+                        e.KeyChar = '\0';
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            Random RandomNumber = new Random();
+            double falseError, missError, sumError;
+            double p1, p2, pc1, pc2, x;
+            double sigm1, sigm2; 
+            double expectedValue1, expectedValue2;
+            double[] randomExperience1 = new double[MAXPOINTS];
+            double[] randomExperience2 = new double[MAXPOINTS];
+            double[] arrx = new double[500];
+            double[] y1 = new double[500]; 
+            double[] y2 = new double[500];
+            double[] linex = new double[500];
+            double[] liney = new double[500];
+            int i;
+
+            pane.CurveList.Clear();
+            if (textBox1.Text == String.Empty)
+            {
+                if (textBox2.Text == String.Empty)
+                {
+                    MessageBox.Show("Enter the probability!");
+                    return;
+                }
+                else
+                {
+                    pc2 = double.Parse(textBox2.Text);
+                    pc1 = 1 - pc2;
+                    textBox1.Text = pc1.ToString();
+                }
+            }
+            else
+            {
+                if (textBox2.Text == String.Empty)
+                {
+                    pc1 = double.Parse(textBox1.Text);
+                    pc2 = 1 - pc1;
+                    textBox2.Text = pc2.ToString();
+                }
+                else
+                {
+                    pc1 = double.Parse(textBox1.Text);
+                    pc2 = double.Parse(textBox2.Text);
+                    if ((pc1 + pc2) != 1)
+                    {
+                        MessageBox.Show("Sum of probabilities must be equal to 1!");
+                        return;
+                    }
+                }
+            }
+
+            for (i = 0; i < MAXPOINTS; i++) 
+            {
+                randomExperience1[i] = RandomNumber.Next(0, 500) - 100;
+                randomExperience2[i] = RandomNumber.Next(0, 500) + 100;
+            }
+            expectedValue1 = 0;
+            expectedValue2 = 0;
+            for (i = 0; i < MAXPOINTS; i++) 
+            {
+                expectedValue1 = expectedValue1 + randomExperience1[i];
+                expectedValue2 = expectedValue2 + randomExperience2[i];
+            }
+            expectedValue1 /= MAXPOINTS;
+            expectedValue2 /= MAXPOINTS;
+            sigm1 = 0;
+            sigm2 = 0;
+            for (i = 0; i < MAXPOINTS; i++) 
+            {
+                sigm1 = sigm1 + Math.Pow(randomExperience1[i] - expectedValue1, 2); //сигма
+                sigm2 = sigm2 + Math.Pow(randomExperience2[i] - expectedValue2, 2);
+            }
+            sigm1 = Math.Sqrt(sigm1/MAXPOINTS);
+            sigm2 = Math.Sqrt(sigm2/MAXPOINTS);
+            for (i = 0; i < 500; i++) 
+            {
+                y1[i] = 100000*pc1 * (Math.Exp(-0.5*Math.Pow((i-expectedValue1)/sigm1, 2)))/(sigm1*Math.Sqrt(2*3.14));
+                y2[i] = 100000*pc2 * (Math.Exp(-0.5*Math.Pow((i-expectedValue2)/sigm2, 2)))/(sigm2*Math.Sqrt(2*3.14));
+                arrx[i] = i;
+            }
+            x = -100;
+            p1 = 1;
+            p2 = 0;
+
+            falseError = 0;
+            if (pc2 != 0)
+                while (p2 < p1) 
+                {
+                    p1 = pc1 * (Math.Exp(-0.5*Math.Pow((x-expectedValue1)/sigm1, 2)))/(sigm1*Math.Sqrt(2*3.14));
+                    p2 = pc2 * (Math.Exp(-0.5*Math.Pow((x-expectedValue2)/sigm2, 2)))/(sigm1*Math.Sqrt(2*3.14));
+                    falseError += p2*0.001;
+                    x += 0.001;
+                }
+            PointPairList listLine = new PointPairList();
+            if (x > 0 && x < 250) 
+            {
+                for (i = 0; i < 250; i++) 
+                    listLine.Add(x, i);
+            }
+            LineItem line = pane.AddCurve("", listLine, Color.Blue, SymbolType.None);
+
+
+            missError = 0;
+            while(x < 1000) 
+            {
+                p1 = (Math.Exp(-0.5*Math.Pow((x-expectedValue1)/sigm1, 2)))/(sigm1*Math.Sqrt(2*3.14));
+                p2 = (Math.Exp(-0.5*Math.Pow((x-expectedValue2)/sigm2, 2)))/(sigm1*Math.Sqrt(2*3.14));
+                missError += p1 * 0.001 * pc1;
+                x += 0.001;
+            }
+            if (pc1 == 0) 
+            {
+                falseError = 1;
+                missError = 0;
+                sumError = 1;
+            }
+            if (pc2 == 0) 
+            {
+                falseError = 0;
+                missError = 0;
+                sumError = 0;
+            }
+            else 
+            {
+                falseError /= pc1;
+                missError /= pc1;
+            }
+            sumError = falseError + missError;
+            textBoxFalseAlarm.Text = falseError.ToString();
+            textBoxMissingError.Text = missError.ToString();
+            textBoxTotalError.Text = sumError.ToString();
+
+            PointPairList listCurve1 = new PointPairList();
+            for (i = 0; i < 500; i++)
+                listCurve1.Add(arrx[i], y2[i]);
+            LineItem curve1 = pane.AddCurve("", listCurve1, Color.Black, SymbolType.None);
+
+            PointPairList listCurve2 = new PointPairList();
+            for (i = 0; i < 500; i++)
+                listCurve2.Add(arrx[i], y1[i]);
+            LineItem curve2 = pane.AddCurve("", listCurve2, Color.Black, SymbolType.None);
+
+            zedGraphControl.AxisChange();
+            zedGraphControl.Invalidate();
+        }
+    }
+}
